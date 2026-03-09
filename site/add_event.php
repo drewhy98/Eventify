f7e4117998acb51af7be82871396c4148bcb8dcc
drewhy98 <150239451+drewhy98@users.noreply.github.com>
2026-03-09 10:18:13 +0000

    Create add_event.php

---
+++ site/add_event.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+include_once "includes/dbconnect.php";
+?>
+
+<?php include 'includes/header.php'; ?>
+
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.8/css/jquery.dataTables.min.css">
+
+<h2 class="page-title">Events Table</h2>
+
+<div class="table-container">
+
+<table id="eventsTable" class="display">
+<thead>
+<tr>
+    <th>Title</th>
+    <th>Location</th>
+    <th>Date</th>
+    <th>Price</th>
+    <th></th>
+</tr>
+</thead>
+
+<tbody>
+
+<?php
+$stmt = $db->query("SELECT * FROM events ORDER BY event_date ASC");
+$events = $stmt->fetchAll();
+
+foreach ($events as $event):
+?>
+
+<tr>
+
+<td><?= htmlspecialchars($event['title']); ?></td>
+
+<td><?= htmlspecialchars($event['place']); ?></td>
+
+<td><?= date("F j, Y", strtotime($event['event_date'])); ?></td>
+
+<td>£<?= number_format($event['price'], 2); ?></td>
+
+<td>
+<form method="post" action="buy_ticket.php">
+<input type="hidden" name="event_id" value="<?= $event['id']; ?>">
+<button class="btn">Buy Ticket</button>
+</form>
+</td>
+
+</tr>
+
+<?php endforeach; ?>
+
+</tbody>
+</table>
+
+</div>
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
+
+<script>
+$(document).ready(function() {
+    $('#eventsTable').DataTable({
+        pageLength: 10,
+        order: [[2, "asc"]] // sort by date
+    });
+});
+</script>
+
+<?php include 'includes/footer.php'; ?>
